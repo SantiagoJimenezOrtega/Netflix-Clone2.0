e2052ad8b59562b35973679d6f320fdfb803d676
santiago jimenez <santoducol@gmail.com>
2024-06-25 15:28:01 -0500

    modified: components/Thumbnail.tsx added rounded

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function Thumbnail({ movie }: Props) {
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 hover:border-white hover:border-2">
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 hover:border-white hover:border-2 rounded">
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path

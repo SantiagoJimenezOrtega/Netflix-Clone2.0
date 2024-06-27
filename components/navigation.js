c4ead4ada50f192da1a6e37685bf82e9d3a3b157
santiago jimenez <santoducol@gmail.com>
2024-06-27 17:52:51 -0500

    Changes to be committed: 	modified:   components/Thumbnail.tsx 	deleted:    components/navigation.js 	deleted:    src/Js/arrowhead.js

--- components/navigation.js
+++
@@ -1,8 +0,0 @@
-// navigation.js
-import { initArrowNavigation } from "@arrow-navigation/core";
-
-const navigationApi = initArrowNavigation({
-  preventScroll: true,
-});
-
-export default navigationApi;
